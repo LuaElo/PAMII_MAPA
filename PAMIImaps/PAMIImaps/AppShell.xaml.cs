cc6639e9004dc757188c8e0bf8eec4d1638a416e
eloahluara <eloahluaraa123@gmail.com>
2024-09-17 12:35:34 -0300

    Fazendo e usando api mapa

---
+++ PAMIImaps/PAMIImaps/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PAMIImaps
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PAMIImaps/PAMIImaps/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace PAMIImaps
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //inicialização do mapa 
+            var location=new Location(-23.51979722880348, -46.596486004008256);
+            var mapSpan = new MapSpan(location, 0.01,0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
